6f22f52c49f390b3d45dc1a931a4960005ab2755
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-07-08 21:49:41 -0400

    Add let method to Context class for defining memoized variables

--- lib/rspec_lite/context.rb
+++ lib/rspec_lite/context.rb
@@ -4,10 +4,19 @@ module RspecLite
   class Context
     def initialize(description)
       @description = description
+      @lets = {}
+    end
+
+    def let(name, &block)
+      @lets[name] = block
+
+      define_singleton_method(name) do
+        @memoized ||= {}
+        @memoized[name] ||= instance_eval(&@lets[name])
+      end
     end
 
     def run
-
     end
   end
 end
