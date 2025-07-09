962339881631c85bce56335d11fe8867480e1673
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-07-08 21:49:45 -0400

    Refactor RspecLite spec to test memoization behavior of let method

--- spec/rspec_lite_spec.rb
+++ spec/rspec_lite_spec.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
-RspecLite.describe 'Array' do
+RspecLite.describe 'Let Test' do
+  let(:number) { 42 }
 
+  # Test that number returns the same object_id (proving it's memoized/singleton)
+  def test_singleton_behavior
+    first_call = number
+    second_call = number
+    third_call = number
+
+    puts "First call object_id: #{first_call.object_id}"
+    puts "Second call object_id: #{second_call.object_id}"
+    puts "Third call object_id: #{third_call.object_id}"
+    puts "Are they the same object? #{first_call.equal?(second_call)}"
+    puts "Values: #{first_call}, #{second_call}, #{third_call}"
+  end
+
+  test_singleton_behavior
 end
