a4b25c71c0aeaafcd6175fe5542d333a20c68362
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-07-08 23:53:13 -0400

    Add before and after hooks to Context class for enhanced test setup

--- lib/rspec_lite/context.rb
+++ lib/rspec_lite/context.rb
@@ -5,9 +5,20 @@ module RspecLite
     def initialize(description)
       @description = description
       @lets = {}
+      @before_hooks = []
+      @after_hooks = []
+    end
+
+    def before(&block)
+      @before_hooks << block
+    end
+
+    def after(&block)
+      @after_hooks << block
     end
 
     def let(name, &block)
+      @lets ||= {}
       @lets[name] = block
 
       define_singleton_method(name) do
@@ -17,6 +28,7 @@ module RspecLite
     end
 
     def run
+      pp 'Before hooks', @before_hooks, 'After hooks', @after_hooks
     end
   end
 end
