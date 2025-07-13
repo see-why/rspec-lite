4443d94dc4890a5be6d03924c3ed4fdd3b4036d8
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-07-12 21:12:18 -0400

    Add tests for array behavior in My Array description

--- spec/rspec_lite_spec.rb
+++ spec/rspec_lite_spec.rb
@@ -14,7 +14,26 @@ RspecLite.describe 'Hook Test' do
 end
 
 RspecLite.describe 'My Array' do
-  it 'is empty' do
+  let(:array) { [1, 2, 3] }
 
+  before do
+    puts 'Running setup before each test'
+  end
+
+  after do
+    puts 'Running teardown after each test'
+  end
+
+  it 'has the correct size' do
+    expect(array.size).to(eq(3))
+  end
+
+  it 'contains the number 2' do
+    expect(array.include?(2)).to(eq(true))
+  end
+
+  it 'can be appended to' do
+    array << 4
+    expect(array.size).to(eq(5))
   end
 end
